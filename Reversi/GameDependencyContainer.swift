63741d122ebe75bce2be88d47069090386a750da
YoshikiTsukada <tsukadayyy@gmail.com>
2021-02-06 23:12:36 +0900

    feat: create GameDependencyContainer

---
+++ Reversi/GameDependencyContainer.swift
@@ -0,0 +1,31 @@
+//
+//  GameDependencyContainer.swift
+//  Reversi
+//
+//  Created by Yoshiki Tsukada on 2021/02/06.
+//
+
+protocol GameDependencyContainerProtocol {
+    func makeGameViewController() -> GameViewController
+}
+
+class GameDependencyContainer: GameDependencyContainerProtocol {
+    func makeGameViewController() -> GameViewController {
+        let cellViewModels = makeCellViewModels()
+        let boardViewModel = makeBoardViewModel(cellViewModels: cellViewModels)
+        let gameViewModel = makeGameViewModel(gameModel: GameModel(), boardViewModel: boardViewModel)
+        return .init(viewModel: gameViewModel, boardViewModel: boardViewModel, cellViewModels: cellViewModels)
+    }
+    
+    func makeGameViewModel(gameModel: GameModelProtocol, boardViewModel: BoardViewModelProtocol) -> GameViewModelProtocol {
+        GameViewModel(gameModel: gameModel, boardViewModel: boardViewModel)
+    }
+    
+    func makeBoardViewModel(cellViewModels: [CellViewModelProtocol]) -> BoardViewModelProtocol {
+        BoardViewModel(cellViewModels: cellViewModels)
+    }
+    
+    func makeCellViewModels() -> [CellViewModelProtocol] {
+        .init(repeating: CellViewModel(), count: AppConst.dimention * AppConst.dimention)
+    }
+}
