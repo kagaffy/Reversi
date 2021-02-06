05dd0c470b7fab861a0dad4d704445e93370b242
YoshikiTsukada <tsukadayyy@gmail.com>
2021-02-06 23:33:58 +0900

    refactor: stop using storyboard

--- Reversi/GameViewController.swift
+++ Reversi/GameViewController.swift
@@ -27,7 +27,7 @@ final class GameViewController: UIViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return nil
     }
     
     override func viewDidLoad() {
