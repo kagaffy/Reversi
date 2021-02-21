c697570881f820358ed75461092d556d9c2b6d8c
YoshikiTsukada <tsukadayyy@gmail.com>
2021-02-21 15:47:18 +0900

    refactor: GameDependencyContainer

--- Reversi/GameDependencyContainer.swift
+++ Reversi/GameDependencyContainer.swift
@@ -9,23 +9,33 @@ protocol GameDependencyContainerProtocol {
     func makeGameViewController() -> GameViewController
 }
 
-class GameDependencyContainer: GameDependencyContainerProtocol {
+final class GameDependencyContainer: GameDependencyContainerProtocol {
+    private let cellViewModels: [CellViewModelProtocol]
+    private let boardViewModel: BoardViewModelProtocol
+    private let gameModel: GameModelProtocol
+    private let gameViewModel: GameViewModelProtocol
+    
+    init() {
+        func makeCellViewModels() -> [CellViewModelProtocol] {
+            (0 ..< AppConst.dimention * AppConst.dimention).map { _ in CellViewModel() }
+        }
+        func makeBoardViewModel(cellViewModels: [CellViewModelProtocol]) -> BoardViewModelProtocol {
+            BoardViewModel(cellViewModels: cellViewModels)
+        }
+        func makeGameModel() -> GameModelProtocol {
+            GameModel()
+        }
+        func makeGameViewModel(gameModel: GameModelProtocol, boardViewModel: BoardViewModelProtocol) -> GameViewModelProtocol {
+            GameViewModel(gameModel: gameModel, boardViewModel: boardViewModel)
+        }
+        
+        cellViewModels = makeCellViewModels()
+        boardViewModel = makeBoardViewModel(cellViewModels: cellViewModels)
+        gameModel = makeGameModel()
+        gameViewModel = makeGameViewModel(gameModel: gameModel, boardViewModel: boardViewModel)
+    }
+    
     func makeGameViewController() -> GameViewController {
-        let cellViewModels = makeCellViewModels()
-        let boardViewModel = makeBoardViewModel(cellViewModels: cellViewModels)
-        let gameViewModel = makeGameViewModel(gameModel: GameModel(), boardViewModel: boardViewModel)
         return .init(viewModel: gameViewModel, boardViewModel: boardViewModel, cellViewModels: cellViewModels)
-    }
-    
-    func makeGameViewModel(gameModel: GameModelProtocol, boardViewModel: BoardViewModelProtocol) -> GameViewModelProtocol {
-        GameViewModel(gameModel: gameModel, boardViewModel: boardViewModel)
-    }
-    
-    func makeBoardViewModel(cellViewModels: [CellViewModelProtocol]) -> BoardViewModelProtocol {
-        BoardViewModel(cellViewModels: cellViewModels)
-    }
-    
-    func makeCellViewModels() -> [CellViewModelProtocol] {
-        (0 ..< AppConst.dimention * AppConst.dimention).map { _ in CellViewModel() }
     }
 }
